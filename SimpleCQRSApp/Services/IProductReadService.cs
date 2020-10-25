59a1a1c84b6fae04d66b68cc0f837445b9392b02
Akimmax <Akimmax>
2020-10-25 09:04:41 +0300

    add services

---
+++ SimpleCQRSApp/Services/IProductReadService.cs
@@ -0,0 +1,17 @@
+﻿using SimpleCQRSApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SimpleCQRSApp.Services
+{
+    public interface IProductReadService
+    {
+        Task<ProductDto> GetByIdAsync(string id);
+
+        Task<IEnumerable<ProductDto>> FindAllAsync(Expression<Func<ProductDto, bool>> predicate);
+
+        Task<IEnumerable<ProductDto>> GetItemsOfAsync(string cartId);
+    }
+}
---
+++ SimpleCQRSApp/Services/IProductWriteService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleCQRSApp.Domain;
+using System.Threading.Tasks;
+
+namespace SimpleCQRSApp.Services
+{
+    public interface IProductWriteService
+    {
+        Task<string> CreateProductAsync(string newCode, string newImageSource, string newProductUrl, string newDescription,  int newPriceValue);
+
+        Task UpdateProductAsync(string productId, string newCode, string newImageSource, string newProductUrl, string newDescription, int newPriceValue);
+
+        Task ChangeProductPriceAsync(string productId, int newPriceValue);
+    }
+}
